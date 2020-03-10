c9aa914f5396f2b59996c88d3d865cf3d82c2eba
John Connor <paulbratslavsky@Johns-MacBook-Pro.local>
2020-03-09 23:57:05 -0500

    first commit

---
+++ WebApplication/Controllers/RoomsController.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class RoomsController : ControllerBase
+    {
+        [HttpGet(Name = nameof(GetRooms))]
+        public IActionResult GetRooms()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
